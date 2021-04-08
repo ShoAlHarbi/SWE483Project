1ee3c217e5cdb11323a640ae139da0c465fd2b32
HaneenAlmohaimeed <60116673+halmohaimeed@users.noreply.github.com>
2021-04-08 18:25:59 +0300

    update bootaction

--- app/src/main/java/com/example/swe483project/BootCompletedActionReceiver.java
+++ app/src/main/java/com/example/swe483project/BootCompletedActionReceiver.java
@@ -3,22 +3,27 @@ package com.example.swe483project;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 
 public class BootCompletedActionReceiver extends BroadcastReceiver {
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
         if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             Toast.makeText(context, "boote completed", Toast.LENGTH_LONG).show();
-            Intent verifyPage = new Intent(context, VerificationActivity.class);
-            verifyPage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(verifyPage);
         } else {
             Toast.makeText(context, "sim changed", Toast.LENGTH_LONG).show();
+            //compare old with new sim card
         }
+        Intent verifyPage = new Intent(context, VerificationActivity.class);
+        verifyPage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startForegroundService(verifyPage);
 
     }
 }
