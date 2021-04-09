be494a44e805046895b1d8c3114f6fb664245f6b
thisisRahaf <60599531+thisisRahaf@users.noreply.github.com>
2021-04-09 18:24:46 +0300

    updates

--- app/src/main/java/com/example/swe483project/UpdateEmailActivity.java
+++ app/src/main/java/com/example/swe483project/UpdateEmailActivity.java
@@ -1,4 +1,18 @@
 package com.example.swe483project;
 
-public class UpdateEmailActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateEmailActivity extends AppCompatActivity {
+
+    String userEmail;
+    String userSIM;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.updateemail_layout);
+    }
 }
