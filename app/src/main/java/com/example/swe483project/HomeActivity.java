1deb70859351e9b3d0dffab6fd3fdabdb07fc0f7
thisisRahaf <60599531+thisisRahaf@users.noreply.github.com>
2021-04-09 18:19:59 +0300

    receive data from intent

--- app/src/main/java/com/example/swe483project/HomeActivity.java
+++ app/src/main/java/com/example/swe483project/HomeActivity.java
@@ -6,9 +6,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    String userEmail;
+    String userSIM;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
+        userEmail = getIntent().getStringExtra("userEmail");
+        userSIM = getIntent().getStringExtra("userSIM");
     }
 }
